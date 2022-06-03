d01421d44a73557aa6e0cd1b3ff177e492885df9
OdilonLimaNeto <odilongeronimoo@gmail.com>
2022-06-03 12:19:44 -0500

    ✨ create compare fileds validation

---
+++ src/validation/validators/compare-fields-validation.ts
@@ -0,0 +1,15 @@
+import { InvalidParamError } from "../../presentation/errors/invalid-param-error";
+import Validation from "../../presentation/protocols/validation";
+
+export class CompareFieldsValidation implements Validation {
+  constructor(
+    private readonly fieldName: string,
+    private readonly fieldToCompareToName: string
+  ) {}
+
+  validate(input: any): any {
+    if (input[this.fieldName] !== input[this.fieldToCompareToName]) {
+      return new InvalidParamError(this.fieldToCompareToName);
+    }
+  }
+}
