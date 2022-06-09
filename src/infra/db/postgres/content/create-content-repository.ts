1e304b67341abed8676f230307083391bae53f34
OdilonLimaNeto <odilongeronimoo@gmail.com>
2022-06-08 22:37:24 -0500

    ✨ create new content repository implementation

---
+++ src/infra/db/postgres/content/create-content-repository.ts
@@ -0,0 +1,12 @@
+import { getRepository } from 'typeorm';
+import CreateContentRepository from "../../../../data/protocols/create-content-repository";
+import Content from "../../../../domain/entities/content";
+import CreateContentDTO from "../../../../domain/usecases/dtos/create-content";
+import ContentEntity from "./content-entity";
+
+export default class CreateContentPostgresRepository implements CreateContentRepository {
+  async create(createContentDTO: CreateContentDTO): Promise<Content> {
+    const contentEntity = getRepository<ContentEntity>(ContentEntity);
+    return await contentEntity.save(contentEntity.create(createContentDTO));
+  }
+}
