4fd261f6b1b400217762ff8f7c915123aca0d945
OdilonLimaNeto <odilongeronimoo@gmail.com>
2022-06-08 22:41:32 -0500

    🎨 improve import file

--- src/infra/db/postgres/content/create-content-repository.ts
+++ src/infra/db/postgres/content/create-content-repository.ts
@@ -2,7 +2,7 @@ import { getRepository } from 'typeorm';
 import CreateContentRepository from "../../../../data/protocols/create-content-repository";
 import Content from "../../../../domain/entities/content";
 import CreateContentDTO from "../../../../domain/usecases/dtos/create-content";
-import ContentEntity from "./content-entity";
+import ContentEntity from "./entities/content-entity";
 
 export default class CreateContentPostgresRepository implements CreateContentRepository {
   async create(createContentDTO: CreateContentDTO): Promise<Content> {
