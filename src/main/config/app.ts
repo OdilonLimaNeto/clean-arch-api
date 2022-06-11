401e1ce8b0fdf64e4c3f4a3e7ea3d86be6e54883
OdilonLimaNeto <odilongeronimoo@gmail.com>
2022-06-10 21:53:53 -0500

    ✨ ensure content route return an content on success

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 import setupMiddlewares from './midlewares';
+import setupRoutes from './routes';
 
 const app = express();
 setupMiddlewares(app)
+setupRoutes(app)
 
 export default app;
