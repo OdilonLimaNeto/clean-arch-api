fd80b4b712a0bda1017ee1dcdc70b5cdcc81e793
OdilonLimaNeto <odilongeronimoo@gmail.com>
2022-06-14 20:37:54 -0500

    🎨 improve jest config

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
   roots: ["<rootDir>/src"],
   collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
   coverageDirectory: "coverage",
-  preset: '@shelf/jest-mongodb',
   testEnvironment: "node",
   transform: {
     ".+\\.ts$": "ts-jest",
