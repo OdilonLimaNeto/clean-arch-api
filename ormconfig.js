0268c7bcb73e593b2f321edfd8ae49cd3a9c8a58
OdilonLimaNeto <odilongeronimoo@gmail.com>
2022-06-18 01:00:09 -0500

    🎨 improve new port to connect database

--- ormconfig.js
+++ ormconfig.js
@@ -3,7 +3,7 @@ const config = [
     name: 'default',
     type: 'postgres',
     host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT || 5433,
+    port: process.env.POSTGRES_PORT || 5432,
     username: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
