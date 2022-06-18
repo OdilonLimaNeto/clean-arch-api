7b5ff55e5c8c81d7f26416965088e6561b0bc2d9
OdilonLimaNeto <odilongeronimoo@gmail.com>
2022-06-18 01:02:50 -0500

    ✨ create new content validation factory

---
+++ src/main/factories/controllers/content/content-validation/content-validation-factory.ts
@@ -0,0 +1,11 @@
+import Validation from "../../../../../presentation/protocols/validation"
+import { RequiredFieldValidation } from "../../../../../validation/validators"
+import { ValidationComposite } from "../../../../../validation/validators/validation-composite"
+
+export const makeContentValidation = (): ValidationComposite => {
+  const validations: Validation[] = []
+  for (const field of [ 'title', 'description', 'thumbnail', 'published', 'sourceDuration', 'sourceSize' ]) {
+    validations.push(new RequiredFieldValidation(field))
+  }
+  return new ValidationComposite(validations)
+}
